f5dc55a0a8db783d0e031eb714d65584f0c62b57
ArComet <fzulinxin@163.com>
2020-10-10 08:43:01 -0400

    hint

--- game.cpp
+++ game.cpp
@@ -23,43 +23,51 @@ struct CardModel{
 			0-王炸/火箭 1-炸弹 2-单牌 3-对子 
 			4-三张 5-三带一 6-三带二 7-顺子 
 			8-双顺 9-三顺 10-飞机 11-四带二 
-			12 void -1 error 
+			12 NULL -1 error 
 		type2牌型附加信息 
 		val特征值 								*/
+	//检查 0-王炸/火箭
 	void check0(){
 		if (CardNum!=2) return;
 		if (Card[14]==1 && Card[15]==1) type1=0,val=0;
 	}
+	//检查 1-炸弹
 	void check1(){
 		if (CardNum!=4) return;
 		for (int i=1; i<RANGE; i++)
 			if (Card[i]==4) type1=1,val=RANK[i];
 	}
+	//检查 2-单牌
 	void check2(){
 		if (CardNum!=1) return;
 		for (int i=1; i<RANGE; i++)
 			if (Card[i]==1) type1=2,val=RANK[i];	
 	}
+	//检查 3-对子
 	void check3(){
 		if (CardNum!=2) return;
 		for (int i=1; i<RANGE; i++)
 			if (Card[i]==2) type1=3,val=RANK[i];	
 	}
+	//检查 4-三张
 	void check4(){
 		if (CardNum!=3) return;
 		for (int i=1; i<RANGE; i++)
 			if (Card[i]==3) type1=4,val=RANK[i];	
 	}
+	//检查 5-三带一
 	void check5(){
 		if (CardNum!=4) return;
 		for (int i=1; i<RANGE; i++)
 			if (Card[i]==3) type1=5,val=RANK[i];	
 	}
+	//检查 6-三带二
 	void check6(){
 		if (CardNum!=5) return;
 		for (int i=1; i<RANGE; i++)
 			if (Card[i]==3) type1=6,val=RANK[i];	
 	}
+	//检查 7-顺子
 	void check7(){
 		int l=13,r=1;
 		for (int i=1; i<=13; i++)
@@ -74,6 +82,7 @@ struct CardModel{
 		type2=r-l+1;
 		val=l;
 	}
+	//检查 8-双顺
 	void check8(){
 		int l=13,r=1;
 		for (int i=1; i<=13; i++)
@@ -88,6 +97,7 @@ struct CardModel{
 		type2=r-l+1;
 		val=l;
 	}
+	//检查 9-三顺
 	void check9(){
 		int l=13,r=1;
 		for (int i=1; i<=13; i++)
@@ -102,6 +112,7 @@ struct CardModel{
 		type2=r-l+1;
 		val=l;
 	}
+	//检查 10-飞机
 	void check10(){
 		int l=13,r=1;
 		for (int i=1; i<=13; i++)
@@ -120,15 +131,18 @@ struct CardModel{
 		type2=r-l+1;
 		val=l;
 	}
+	//检查 11-四带二
 	void check11(){
 		if (CardNum!=6) return;
 		for (int i=1; i<RANGE; i++)
 			if (Card[i]==4) type1=11,val=RANK[i];	
 	}
+	//检查 12 NULL
 	void check12(){
 		if (CardNum!=0) return;
 		type1=12;
 	}
+	//调用所有的检查函数，确定当前牌型
 	void TypeCheck(){
 		type1=-1;type2=0;
 		check0();check1();check2();
@@ -137,12 +151,14 @@ struct CardModel{
 		check9();check10();check11();
 		check12();
 	}
+	//空构造函数
 	CardModel(){
 		CardNum=0;
 		for (int i=1; i<RANGE; i++) Card[i]=0;
 		type1=12;
 		ModelName="NULL";
 	}
+	//构造函数，由输入的数组生成对应牌型
 	CardModel(int _Card[RANGE]){
 		CardNum=0;
 		for (int i=1; i<RANGE; i++){
@@ -151,9 +167,9 @@ struct CardModel{
 		}
 		TypeCheck();
 		if (type1==-1) ModelName="无对应牌型";
-		else ModelName=MODELNAME[type1];
+		else ModelName=MODELNAME[type1];//由type1确定牌型名
 	}
-	// -1:can't_cmp  0:>=  1:<
+	// -1:无法比较  0:>=  1:<
 	friend int cmp_CardModel(CardModel a,CardModel b){
 		if (a.type1==12){
 			if (b.type1==12) return 0;
@@ -174,24 +190,27 @@ struct Game{
 	int Card[RANGE];
 	CardModel PreModel;
 	bool flag_lord;
-
+	//构造函数，清空Card和PreModel
 	Game(){
 		for (int i=1; i<RANGE; i++) Card[i]=0;
 		PreModel=CardModel();
 	}
+	/*	洗牌发牌函数，由某一玩家调用，传入两个数组，
+		调用后设置此玩家的Card，并其他玩家的Card保存在参量中导出
+		其他玩家调用SetCard将Card信息导入			*/
 	void ShuffleCard(int Card1[RANGE],int Card2[RANGE]){
-		for (int i=1; i<RANGE; i++) Card[i]=Card1[i]=Card2[i]=0;
+		for (int i=1; i<RANGE; i++) Card[i]=Card1[i]=Card2[i]=0;//清空
 		int Deck[60];
-		for (int i=1; i<=13; i++)
-			for (int j=0; j<4; j++) 
+		for (int i=1; i<=13; i++)//数值1-13
+			for (int j=0; j<4; j++)//每种4张
 				Deck[4*(i-1)+j]=i;
-		Deck[52]=14;
-		Deck[53]=15;
-		srand(time(0));
-		random_shuffle(Deck,Deck+54);
-		for (int i=0; i<17; i++) Card[Deck[i]]++;
-		for (int i=17; i<17+17; i++) Card1[Deck[i]]++;
-		for (int i=17+17; i<17+17+17; i++) Card2[Deck[i]]++;
+		Deck[52]=14;//小王
+		Deck[53]=15;//大王
+		srand(time(0));//取当前时间为随机种子
+		random_shuffle(Deck,Deck+54);//对54张牌随机重排
+		for (int i=0; i<17; i++) Card[Deck[i]]++;//0-16发给此玩家
+		for (int i=17; i<17+17; i++) Card1[Deck[i]]++;//17-33发给1
+		for (int i=17+17; i<17+17+17; i++) Card2[Deck[i]]++;//34-53发给2
 	}
 	void SetCard(int _Card[RANGE]){
 		for (int i=1; i<RANGE; i++) Card[i]=_Card[i];
